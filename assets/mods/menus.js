ef3ae40e5c5b42fd5f983fb53cf34c2ac0d49513
Daniel Lam <Dann.lam@gmail.com>
2023-04-29 12:33:21 -0700

    questions initialized

---
+++ assets/mods/menus.js
@@ -0,0 +1,33 @@
+// GIVEN a command-line application that accepts user input
+const express = require('express');
+const inquirer = require('inquirer');
+const mysql = require('mysql2');
+
+const app = express();
+
+let questions = [
+    {
+        type:" list",
+        name: "mainMenu",
+        message:"Choose an Option",
+        choices:[ "View All Departments", "View All Roles", "View All Employees", "Add a Department", "Add a Role", "Add An Employee", "Update an Employee"],
+    },
+];
+
+
+
+const CLI = () => {
+    inquirer.prompt(questions).then((output) => {
+        console.log(output);
+    })
+    .catch((error)=> {
+        if (error) {
+            console.log(error)
+            throw new Error ("Ohhh nooo");
+        }
+    })
+};
+
+module.exports = {
+CLI,
+};
