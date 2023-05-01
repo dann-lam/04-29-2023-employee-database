169bb3eeea041ca86a3eb83123a7057d00218e8b
Daniel Lam <Dann.lam@gmail.com>
2023-04-30 18:28:56 -0700

    Adding comments

--- assets/mods/queryhandler.js
+++ assets/mods/queryhandler.js
@@ -1,10 +1,5 @@
 const mysql = require('mysql2');
-
 const inquirer = require('inquirer');
-
-// var { CLI } = require('./menus')
-//For some forsaken reason I do not understand, I cannot for the life of me
-//Summon the CLI.
 
 
 const db = mysql.createConnection(
@@ -16,7 +11,8 @@ const db = mysql.createConnection(
     },
     console.log(`Connected to the employee_db database.`)
 );
-
+//Question set for specific inquirer prompts.
+//Move these into their own file in future.
 const departmentQuestions = [
     {
         type: "input",
@@ -24,6 +20,7 @@ const departmentQuestions = [
         message: "Insert a new department."
     },
 ];
+
 const roleQuestions = [
     {
         type: "input",
@@ -63,25 +60,20 @@ const employeeQuestions = [
         message: "Insert a Manager. (ID)"
     },
 ];
-// UPDATE employee SET role_id = ? WHERE employee.id = ?
-
-
-
 
 
 //Modular function designed to handle simple view functions.
 const viewQuery = (sql) => {
-    //Gives us a promise
 
         return db.query(sql, function (err, results) {
         if (results) {
             console.table(results)
-            return results;
+            //Ends the connection.
+            db.end();
 
         } else if (err) {
             console.log(err)
         }
-        db.end();
     })
 
 }
@@ -91,8 +83,11 @@ const addQuery = (sql, type) => {
     //Determine what questions is being asked based on SQL input.
     let questions;
     let option;
+    //The way I'm handling our type selection is suboptimal RN.
+    //I could use it to directly influence our "option" which we chose, could DRY up the code here.
     if(type == "department"){
         questions = departmentQuestions;
+        //I set the option here so once we've added our new content we can immediately re-draw the relevent content.
         option = 'SELECT * FROM department'
     } else if (type == "role"){
         questions = roleQuestions;
@@ -113,12 +108,13 @@ const addQuery = (sql, type) => {
         output = (Object.values(answers));
 
             //output is already an array with 3 items in it.
-            //If I don't encase output like [[blah,blah,blah]] IT HATES IT
-            //UGH. WHy??
+            //encase output like E.G. : [[blah,blah,blah]] for this to work for a mysterious reason :)
+
 
          db.query(sql, [output], function (err, results) {
             if (results) {
                 console.log(`Returned: ${sql}`);
+                //redraws our final output.
                 viewQuery(option)
             } else if (err) {
                 console.log(sql)
@@ -141,10 +137,9 @@ const updateQuery = (sql) => {
     let employeeList;
     let roleList;
 
-    //We need to do two questions inside of an inquirer prompt.
-    //Before we do that, I want to be able to grab the list of Employees
-    //and the list of roles
-    //and have them as choices.
+//First query will get a list of Employees, saves it to a variable.
+//Second query does the same with a list of roles.
+//Point is to get a list of IDs of names and Roles to be available for selection with the Inquirer CLI prompt.
 
         db.query("SELECT * FROM employee", function (err, results) {
         if (results) {
@@ -154,7 +149,7 @@ const updateQuery = (sql) => {
            db.query("SELECT * FROM role", function (err, results) {
             if (results) {
                roleList = results;
-               //Begin prompting.
+
 
                //Initialize empty arrays for our inquirer prompt questions.
                let employeeListArr = [];
@@ -177,7 +172,7 @@ const updateQuery = (sql) => {
                 roleListArr.push(roleListStr)
                }
 
-
+            //Promp user with our freshly minted choices list.
                inquirer.prompt(
                 [
                     {
@@ -203,10 +198,14 @@ const updateQuery = (sql) => {
 
                 //DB QUERY to change our Employee based on ID to role based on ID.
 
-                db.query(sql, [empID,roleID], function (err, results) {
+                //SQL = 'UPDATE employee SET role_id = ? WHERE employee.id = ?'
+                db.query(sql, [roleID,empID], function (err, results) {
                     if (results) {
                         console.table(results);
                         console.log("Results updated!!");
+                        //Show us the employee table.
+                        viewQuery('SELECT * FROM employee')
+
                     } else if (err) {
                         console.log(err)
                     }
@@ -223,20 +222,8 @@ const updateQuery = (sql) => {
         }
     })
 
-    //Function role Change
-
-
-
-    // console.log("List of Employees:")
-    // viewQuery("SELECT * FROM employee");
-    // console.log("List of Roles:")
-    // viewQuery("SELECT * FROM role");
-
-    //Prompt to choose an ID.
-
-
-
 }
+
 
 //sets our query to the appropriate query, then hands it off to another
 //appropriate function that is handling either views, inserts, updates or deletes.
@@ -246,7 +233,7 @@ const queryHandler = (option) => {
     // db.connect();
     let query;
     console.log(`You choose ${option}`);
-    // WHEN I choose to view all departments
+
     if (option == 'View All Departments') {
         query = 'SELECT * FROM department';
         viewQuery(query);
@@ -256,31 +243,27 @@ const queryHandler = (option) => {
         query = 'SELECT * FROM role'
         viewQuery(query);
 
-    // THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
 
-    // WHEN I choose to view all employees
+
     } else if (option == 'View All Employees') {
         query = 'SELECT * FROM employee'
         viewQuery(query);
-        // THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
 
-    // WHEN I choose to add a department
+
     } else if (option == 'Add a Department') {
         query = 'INSERT INTO department (name) VALUES(?)';
         type = "department";
         addQuery(query, type);
-    // THEN I am prompted to enter the name of the department and that department is added to the database
 
-    // WHEN I choose to add a role
+
     } else if (option == 'Add a Role') {
         query = 'INSERT INTO role (title, salary, department_id) VALUES(?)'
         type = "role";
 
         addQuery(query, type);
-    // THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
 
     }
-    // WHEN I choose to add an employee
+
     else if (option == 'Add An Employee') {
         query = 'INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES(?)'
         type = "employee";
